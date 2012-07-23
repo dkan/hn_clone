ccfa11fe75b0c898433a43dec52e7c1f5c0c5225
Apprentice <apprentice@dbc08.devbootcamp.com>
2012-07-23 12:10:40 -0700

    index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 HnClone::Application.routes.draw do
+
+  root :to => 'articles#index'
+  resources :articles
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
