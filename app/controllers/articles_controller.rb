40f9487cbb2fcfd932cae2e30bdc90b85d437fbd
Apprentice <apprentice@dbc08.devbootcamp.com>
2012-07-23 12:56:06 -0700

    articles#new without flash

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,19 @@
 class ArticlesController < ApplicationController
 
   def index
-    @articles = Article.all
+    @articles = Article.all.reverse
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params[:article])
+    if @article.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,9 @@
 class Article < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :title, :url
+  URL_RULE = /^http/i
+
+
+  validates :url, :presence => true, :uniqueness => true, :format => { :with => URL_RULE, :message => 'Not a valid URL' }
+
+
 end
