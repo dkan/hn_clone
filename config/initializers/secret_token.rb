b96b8b34918b1adde66b402d0a083de354ffc5f0
Apprentice <apprentice@dbc08.devbootcamp.com>
2012-07-23 11:18:36 -0700

    postgres and everything else

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HnClone::Application.config.secret_token = '6764d5c321fab1a29215582e8c127319933e46baa68fc9f71ce57600d723696651a040771739d84a23bfbe24801eba1e50600d8ff66f2b1879d30ff3f8db2d49'
