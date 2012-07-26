68e8f22ecf8acf65d6a38cca191c830d6662e4f8
Apprentice <apprentice@dbc08.local>
2012-07-25 18:57:31 -0700

    obscure collision bug

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,5 +3,5 @@ class Vote < ActiveRecord::Base
   belongs_to :voteable, :polymorphic => true
   belongs_to :user
 
-  validates_uniqueness_of :user_id, :scope => [:voteable_id]
+  validates_uniqueness_of :user_id, :scope => [:voteable_id, :voteable_type]
 end
