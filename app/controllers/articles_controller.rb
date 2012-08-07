bc298ad2e01ae5753b2ddd14ce9809de78ba6282
Damien <damien.kan@gmail.com>
2012-08-07 00:15:17 -0700

    json for iOS

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,45 +1,52 @@
 class ArticlesController < ApplicationController
   before_filter :find_article, :only => [:show, :edit, :update]
 
-  def index
-    @articles = Kaminari.paginate_array(Article.non_flagged_articles.sort_by! { |article| article.score }).page(params[:page]).per(5)
-  end
-
-  def show
-    @comment = @article.comments.new
-  end
-
-  def new
-    @article = Article.new
-    unless signed_in?
-      flash[:error] = 'Please sign in to submit an article'
-      redirect_to new_user_session_path
+    def index
+        respond_to do |format|
+            format.html {
+                @articles = Kaminari.paginate_array(Article.non_flagged_articles.sort_by! { |article| article.score }).page(params[:page]).per(5)
+            }
+            format.json {
+                render :json => Article.non_flagged_articles.sort_by! { |article| article.score }
+            }
+        end
     end
-  end
+    
+    def show
+        @comment = @article.comments.new
+    end
 
-  def create
-    @article = Article.new(params[:article])
-    @article[:user_id] = current_user.id
-    if @article.save
-      flash[:success] = "Thanks for submitting an article"
-      @article.votes.create(:user_id => current_user.id, :value => 1)
-      redirect_to root_path
-    else
-      flash[:error] = "Invalid Submission"
-      render 'new'
+    def new
+        @article = Article.new
+        unless signed_in?
+            flash[:error] = 'Please sign in to submit an article'
+            redirect_to new_user_session_path
+        end
     end
-  end
 
-  def edit
-    if current_user != @article.user
-      flash[:error] = "That article doesn't belong to you!"
-      redirect_to root_path
+    def create
+        @article = Article.new(params[:article])
+        @article[:user_id] = current_user.id
+        if @article.save
+            flash[:success] = "Thanks for submitting an article"
+            @article.votes.create(:user_id => current_user.id, :value => 1)
+            redirect_to root_path
+        else
+            flash[:error] = "Invalid Submission"
+            render 'new'
+        end
     end
-    if @article.too_late_to_edit?
-      flash[:error] = "Can't edit after 15 minutes"
-      redirect_to root_path
+
+    def edit
+        if current_user != @article.user
+            flash[:error] = "That article doesn't belong to you!"
+            redirect_to root_path
+        end
+        if @article.too_late_to_edit?
+            flash[:error] = "Can't edit after 15 minutes"
+            redirect_to root_path
+        end
     end
-  end
 
   def update
     if current_user == @article.user && @article.update_attributes(params[:article])
